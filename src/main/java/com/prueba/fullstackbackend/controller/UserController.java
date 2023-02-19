2b789c0e8d84d241922b15de00145091a872259a
stivennevits <stivennevits16@gmail.com>
2023-02-18 20:19:02 -0500

    se crea get user by id & update user

--- src/main/java/com/prueba/fullstackbackend/controller/UserController.java
+++ src/main/java/com/prueba/fullstackbackend/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
@@ -36,6 +37,21 @@ public class UserController {
 		return userRepository.findById(id)
 				.orElseThrow(()->new UserNotFoundException(id));
 	}
+	
+	@PutMapping("/user/{id}")
+	User updateUser(@RequestBody User newUser, @PathVariable Long id) {
+		
+		return userRepository.findById(id)
+				.map(user -> {
+					user.setUsername(newUser.getUsername());
+					user.setName(newUser.getName());
+					user.setEmail(newUser.getEmail());
+					return userRepository.save(user);
+				
+				}).orElseThrow(()-> new UserNotFoundException(id));
+	}
+	
+	
 	
 	
 	
--- src/main/java/com/prueba/fullstackbackend/exception/UserNotFoundException.java
+++ src/main/java/com/prueba/fullstackbackend/exception/UserNotFoundException.java
@@ -1,6 +1,11 @@
 package com.prueba.fullstackbackend.exception;
 
 public class UserNotFoundException extends RuntimeException{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public UserNotFoundException(Long id) {
 		super("Could not found user with id " + id);
 	}
