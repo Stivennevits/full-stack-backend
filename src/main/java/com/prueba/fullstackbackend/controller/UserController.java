eb6b4abb5fbeeac522df485d8a3510faae0d2999
stivennevits <stivennevits16@gmail.com>
2023-02-18 19:53:04 -0500

    Se crea controller para get userById, con exceptions

--- src/main/java/com/prueba/fullstackbackend/controller/UserController.java
+++ src/main/java/com/prueba/fullstackbackend/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.prueba.fullstackbackend.controller;
 
+import com.prueba.fullstackbackend.exception.UserNotFoundException;
 import com.prueba.fullstackbackend.model.User;
 import com.prueba.fullstackbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +29,12 @@ public class UserController {
 	List<User> getAllUsers(){
 		return userRepository.findAll();
 		
+	}
+	
+	@GetMapping("/user/{id}")
+	User getUserById(@PathVariable Long id) {
+		return userRepository.findById(id)
+				.orElseThrow(()->new UserNotFoundException(id));
 	}
 	
 	
